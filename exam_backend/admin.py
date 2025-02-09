b61b90f6c2b531a8416f2ea447dd3f1e72414b16
Gourav Rathi <159235491+rathi-java@users.noreply.github.com>
2025-02-09 12:11:33 +0530

    Delete exam_backend directory

--- exam_backend/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from exam_backend.models import Professor, Course, Difficulty, Exam, Question
-
-admin.site.register(Professor)
-admin.site.register(Course)
-admin.site.register(Difficulty)
-admin.site.register(Exam)
-admin.site.register(Question)
--- exam_backend/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExamBackendConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'exam_backend'
